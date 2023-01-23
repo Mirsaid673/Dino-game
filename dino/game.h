aec96b0db45881c2330bdac36a77825b15baff13
KirTheGreat <mr.mirsaid899@gmail.com>
2023-01-23 20:00:34 +1100

    game ready

--- dino/game.h
+++ dino/game.h
@@ -5,31 +5,53 @@
 #include "ground.h"
 #include "physics.h"
 #include "enemy.h"
+#include "my_shader.h"
+#include "bg.h"
 
 class Game : public Application
 {
-    Ground ground;
+    float offset = 10;
+    Background background;
     Dino dino;
-    Enemy enemy;
+    Ground ground1 = Ground({-10, -5});
+    Ground ground2 = Ground({10, -5});
+    Enemy enemy1 = Enemy({4 + offset * 0, -3});
+    Enemy enemy2 = Enemy({4  + offset * 1, -3});
+
     void init() override
     {
+        MyShader::initialize();
+        Ground::initialize();
+        Enemy::initialize();
         camera2d.ortho((float)window.getWidth() / (float)window.getHeight() * scale, scale);
         Renderer::setClearColor(glm::vec3(1));
         scene.addNode(physics);
-        scene.addNode(ground);
+        scene.addNode(background);
+        scene.addNode(ground1);
+        scene.addNode(ground2);
         scene.addNode(dino);
-        scene.addNode(enemy);
+        scene.addNode(enemy1);
+        scene.addNode(enemy2);
+    }
+
+    void determinate() override
+    {
+        MyShader::determionate();
     }
 
     float scale = 10.0f;
+    float max_aspect = 1.9f;
     void onResize() override
     {
-        camera2d.ortho((float)window.getWidth() / (float)window.getHeight() * scale, scale);
+        float height = (float)window.getHeight();
+        float width = (float)window.getWidth();
+        window.setMaxSize({height * max_aspect+10, height+10});
+        camera2d.ortho(width / height * scale, scale);
     }
 
     void update() override
     {
         if (input.getKeyDown(GLFW_KEY_ESCAPE) || dino.game_over)
-            running = false;
+            running = false; 
     }
 };
